5a3b6e2acb78b14df321d50b1aa5e2b6b0a12648
giacomo9999 <gmarchesi888@gmail.com>
2021-05-28 19:44:25 -0400

    Working on functionality.

---
+++ script.js
@@ -0,0 +1,26 @@
+import quizData from "./quizData.js";
+
+console.log(quizData[0]);
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll("answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+const loadQuiz = () => {
+  let currentQuizData = quizData[currentQuiz];
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+};
+
+loadQuiz();
