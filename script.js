879d24323cd77bb59ba1d4786cf46eeff5e75ec7
giacomo9999 <gmarchesi888@gmail.com>
2021-05-29 15:50:24 -0400

    Everything working...

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 import quizData from "./quizData.js";
 
-console.log(quizData[0]);
-
 const quiz = document.getElementById("quiz");
-const answerEls = document.querySelectorAll("answer");
+const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
@@ -15,6 +13,8 @@ let currentQuiz = 0;
 let score = 0;
 
 const loadQuiz = () => {
+  deselectAnswers();
+
   let currentQuizData = quizData[currentQuiz];
   questionEl.innerText = currentQuizData.question;
   a_text.innerText = currentQuizData.a;
@@ -22,5 +22,57 @@ const loadQuiz = () => {
   c_text.innerText = currentQuizData.c;
   d_text.innerText = currentQuizData.d;
 };
+
+const deselectAnswers = () => {
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
+};
+
+const getSelected = () => {
+  let answer;
+  answerEls.forEach((el) => {
+    if (el.checked) {
+      answer = el.id;
+    }
+  });
+
+  return answer;
+};
+
+const checkAnswer = () => {
+  const answer = getSelected();
+  if (answer === quizData[currentQuiz].correct) {
+    score += 1;
+    console.log("Correct answer...score: ", score);
+  } else {
+    console.log("Incorrect answer...score: ", score);
+  }
+  currentQuiz += 1;
+};
+
+const checkLastQuestion = () => {
+  if (currentQuiz < quizData.length) {
+    console.log("Index of current question: ", currentQuiz);
+    return true;
+  } else {
+    console.log("Game over...Score:", score);
+    return false;
+  }
+};
+
+submitBtn.addEventListener("click", () => {
+  checkAnswer();
+  if (checkLastQuestion()) {
+    loadQuiz();
+  } else {
+    console.log("Quiz over.");
+    let quizBlock = document.querySelector(".question-block");
+    quizBlock.innerHTML = "";
+    questionEl.innerText = "Quiz Complete.";
+    var scoreText = document.createElement("H1");
+    scoreText.innerHTML = "Score " + score;
+    questionEl.appendChild(scoreText);
+    submitBtn.innerText = "Thanks For Playing";
+  }
+});
 
 loadQuiz();
